c994941e96dfa6dbf7bde0312b94c2eb81320c20
saklainMostake <saklain15-12003@diu.edu.bd>
2022-10-20 05:35:13 +0600

    push

--- src/Pages/Catagory/Catagory/Catagory.js
+++ src/Pages/Catagory/Catagory/Catagory.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import NewsSummaryCard from '../../Shared/NewsSummaryCard/NewsSummaryCard';
 
 const Catagory = () => {
-    const news = useLoaderData()
+    const catagoryNews = useLoaderData()
     return (
         <div>
-            <h2>This is catagory has news: {news.length} </h2>
+            <h2>This is catagory has news: {catagoryNews.length} </h2>
+            {
+                catagoryNews.map(news => <NewsSummaryCard
+                key={news._id}
+                news={news}
+                ></NewsSummaryCard>)
+            }
         </div>
     );
 };
--- src/Pages/News/News/News.js
+++ src/Pages/News/News/News.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 
 const News = () => {
-   
+   const news = useLoaderData()
+   console.log(news)
+   const {title, details, image_url, category_id} = news
     return (
-        <div>
-            <h2>All News</h2>
-        </div>
+        <Card >
+        <Card.Img variant="top" src={image_url} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>
+           {details}
+          </Card.Text>
+          <Link to={`/catagory/${category_id}`}>
+             <Button variant="primary">All News In This Catagory</Button>
+          </Link>
+        </Card.Body>
+      </Card>
     );
 };
 
