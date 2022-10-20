7c40ef5575ea4434d2ad5ac77b04fd8334c1e767
saklainMostake <saklain15-12003@diu.edu.bd>
2022-10-20 07:28:16 +0600

    all done

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 
 const Register = () => {
-    const handleSubmit = event =>{
+    const {createUser} = useContext(AuthContext)
+    const handleOnSubmit = event =>{
         event.preventDefault()
-
+        const form = event.target
+        const name = form.name.value
+        const photoUrl = form.photo.value
+        const email = form.email.value
+        const password = form.password.value
+        console.log(name, photoUrl, email, password)
+        createUser(email, password)
+        .then(result =>{
+            const user = result.user
+            console.log(user)
+            form.reset()
+        })
+        .catch(error =>{
+            console.error(error)
+        })
     }
     return (
-        <Form onSubmit={handleSubmit}>
+        <Form onSubmit={handleOnSubmit}>
         <Form.Group className="mb-3" controlId="formBasicName">
           <Form.Label>Your Name</Form.Label>
           <Form.Control type="text" name='name' placeholder="Enter Your Name" />
@@ -23,7 +39,7 @@ const Register = () => {
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" name='password' placeholder="Password" required/>
+          <Form.Control name='password'  type="password" placeholder="Password" required/>
         </Form.Group>
         <Form.Group className="mb-3" >
         <Form.Text className="text-muted">
