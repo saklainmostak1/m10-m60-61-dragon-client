f2a1bba17df84348a43340c39de4b0c4c3597e9b
saklainMostake <saklain15-12003@diu.edu.bd>
2022-10-21 00:55:37 +0600

    user photo

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,59 +1,89 @@
 import React, { useContext, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 
 const Register = () => {
+  const [accepted, setAccepted] = useState(false)
   const [error, setError] = useState('')
-    const {createUser} = useContext(AuthContext)
-    const handleOnSubmit = event =>{
-        event.preventDefault()
-        const form = event.target
-        const name = form.name.value
-        const photoUrl = form.photo.value
-        const email = form.email.value
-        const password = form.password.value
-        console.log(name, photoUrl, email, password)
-        createUser(email, password)
-        .then(result =>{
-            const user = result.user
-            console.log(user)
-            setError('')
-            form.reset()
-        })
-        .catch(error =>{
-            console.error(error)
-            setError(error.message)
-        })
+  const { createUser,  updateUserProfile  } = useContext(AuthContext)
+  const handleOnSubmit = event => {
+    event.preventDefault()
+    const form = event.target
+    const name = form.name.value
+    const photoURL = form.photo.value
+    const email = form.email.value
+    const password = form.password.value
+    console.log(name, photoURL, email, password)
+    createUser(email, password)
+      .then(result => {
+        const user = result.user
+        console.log(user)
+        setError('')
+        form.reset()
+        handleUpdateUserProfile(name, photoURL)
+        
+      })
+      .catch(error => {
+        console.error(error)
+        setError(error.message)
+      })
+  }
+
+  const handleUpdateUserProfile = (name, photoURL) =>{
+    const profile = {
+      displayName : name,
+      photoURL: photoURL
     }
-    return (
-        <Form onSubmit={handleOnSubmit}>
-        <Form.Group className="mb-3" controlId="formBasicName">
-          <Form.Label>Your Name</Form.Label>
-          <Form.Control type="text" name='name' placeholder="Enter Your Name" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Photo Url</Form.Label>
-          <Form.Control name='photo' type="text" placeholder="Photo Url" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control name='email' type="email" placeholder="Enter email" required />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control name='password'  type="password" placeholder="Password" required/>
-        </Form.Group>
-        <Form.Group className="mb-3" >
+    updateUserProfile(profile) 
+    .then(() =>{})
+    .catch(error =>{
+      console.error(error)
+    })
+  }
+  const handleAccepted = event =>{
+    setAccepted(event.target.checked)
+    console.log(event.target.checked)
+  }
+  return (
+    <Form onSubmit={handleOnSubmit}>
+      <Form.Group className="mb-3" controlId="formBasicName">
+        <Form.Label>Your Name</Form.Label>
+        <Form.Control type="text" name='name' placeholder="Enter Your Name" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Photo Url</Form.Label>
+        <Form.Control name='photo' type="text" placeholder="Photo Url" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control name='email' type="email" placeholder="Enter email" required />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control name='password' type="password" placeholder="Password" required />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check 
+              type="checkbox" 
+              onClick={handleAccepted}
+              label={
+                <>
+                  Accept <Link to='/terms'>Terms And Conditins</Link>
+                </>
+              }/>
+      </Form.Group>
+      <Form.Group className="mb-3" >
         <Form.Text className="text-danger">
-           {error}
-          </Form.Text>
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Register
-        </Button>
-      </Form>
-    );
+          {error}
+        </Form.Text>
+      </Form.Group>
+      <Button variant="primary" type="submit" disabled={!accepted}>
+        Register
+      </Button>
+    </Form>
+  );
 };
 
 export default Register;
--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth'
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth'
 import app from '../../firebase/firebase.config';
 
 export const AuthContext = createContext()
@@ -23,6 +23,11 @@ const AuthProvider = ({ children }) => {
         setLoading(true)
         return signInWithEmailAndPassword(auth, email, password)
     }
+    
+    const updateUserProfile = (profile) =>{
+        return updateProfile(auth.currentUser,profile)
+    }
+
     const logOut = () => {
         setLoading(true)
         return signOut(auth)
@@ -41,7 +46,15 @@ const AuthProvider = ({ children }) => {
 
 
 
-    const authInfo = { user, providerLogin, logOut, createUser, signIn, loading }
+    const authInfo = {
+         user,
+         providerLogin, 
+         logOut, 
+         createUser, 
+         signIn, 
+         loading,
+         updateUserProfile ,
+        }
 
     return (
         <AuthContext.Provider value={authInfo}>
